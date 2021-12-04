a9d7ced94f9c9d0e774e5175d5dfb94a810a6591
Krystian Stopka <kr6stopka@gmail.com>
2021-12-04 12:31:16 +0100

    start zadanie zdalne e-msi

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { FunctionComponent } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+interface AppProps {}
+
+const App: FunctionComponent<AppProps> = () => {
+    return (
+        <div className="wrapper">
+            <h1>Zadanie Zdalne e-MSI - Krystian Stopka</h1>
+        </div>
+    );
+};
 
 export default App;
