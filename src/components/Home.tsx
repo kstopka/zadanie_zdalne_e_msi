c87c1b43da611ca0b571ebb59d22558de521d768
Krystian Stopka <kr6stopka@gmail.com>
2021-12-08 15:23:19 +0100

    description in homePage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,20 @@ import { FunctionComponent } from "react";
 interface HomeProps {}
 
 const Home: FunctionComponent<HomeProps> = () => {
-    return <div className="home">{<h1>Home:</h1>}</div>;
+    return (
+        <div className="home">
+            <h1>Cześć,</h1>
+            <p>
+                Chciałem dodać parę słów wstępu. Strona jest zrobiona w technologii React, której nadal się uczę.
+                Stwierdziłem, iż zadania zrobię w ramach nauki właśnie w tej technologii. Nie do końca potrafiłem zrobić
+                wszystkich zaleconych zadań. Włożyłem w te zadania trochę pracy, mimo iż zaplanowałem sobie tochę więcej
+                czasu niż potrzebowałem to wyskoczyło mi pare prywatych spraw przez co nie wszystko jest skończone. Co
+                do baz danych nie miałem za dużej styczności w połączeniu z frontendem, a tym bardziej MySQL. Samą
+                technologię znam pod względem bazy danych - w przeszłości ze 2 projekty. Z mojej strony mogę jeszcze
+                życzyć miłego dania i prosić oczywiście o feedback mojego kodu.
+            </p>
+        </div>
+    );
 };
 
 export default Home;
